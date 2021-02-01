819380afe7be23f7153bc12ef41599acf1c9395f
rupahli <rupahli.garg@gmail.com>
2021-02-01 18:32:00 +0530

    swagger enabled

--- inventory-management/src/main/java/com/sl/ms/inventorymanagement/InventoryManagementApplication.java
+++ inventory-management/src/main/java/com/sl/ms/inventorymanagement/InventoryManagementApplication.java
@@ -10,9 +10,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableCaching
+@EnableSwagger2
 public class InventoryManagementApplication {
 
     public static void main(String[] args) {
@@ -29,6 +31,9 @@ public class InventoryManagementApplication {
                     .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                     .authorizeRequests()
                     .antMatchers(HttpMethod.POST, "/user").permitAll()
+                    .antMatchers("/swagger-ui.html**").permitAll()
+                    .antMatchers("/v2/api-docs**").permitAll()
+                    .antMatchers("/", "/csrf", "/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**", "/swagger-ui.html", "/webjars/**").permitAll()
                     .anyRequest().authenticated();
         }
     }
